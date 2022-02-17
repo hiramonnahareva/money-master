a49b7b50a50c45a8eb63e1ec3b4f56cb60538c06
Hiramon Nahar Eva <96996000+hiramonnahareva@users.noreply.github.com>
2022-02-17 02:39:44 -0800

    some modify

--- js-file/custom.js
+++ js-file/custom.js
@@ -1,42 +1,69 @@
-
-
-
+// input function part 
 function inputValue (inputId){
   const AmountText = document.getElementById (inputId).value
   const Amount = parseFloat(AmountText)
-  return Amount
+  if (Amount > 0){
+  return Amount ;
+  }
+  else {
+    alert(inputId + ' :' +' please give positive value')
+  }
 }
-// function ()
-
+// calculator part 
 document.getElementById ('calc-btn').addEventListener('click' ,function (){
- const incomeAmount =  inputValue('income')
- const foodAmount  =  inputValue ('food')
- const rentAmount =  inputValue('rent')
- const clothesAmount =  inputValue('clothes')
-//  document.getElementById ('total-expeses').innerText = foodamount + rentamount + clothesamount ;
-
-//  function balance(){
+  const incomeAmount =  inputValue('income')
+  const foodAmount  =  inputValue ('food')
+  const rentAmount =  inputValue('rent')
+  const clothesAmount =  inputValue('clothes')
+  // total expanses part
   const expensesText = document.getElementById ('total-expeses');
-  const expensesAmount = parseFloat (expensesText) 
   const expensesTotal = foodAmount + rentAmount + clothesAmount; 
+  if (expensesTotal>0){
   expensesText.innerText = expensesTotal ;
+  document.getElementById ('expenses-text').style.display = 'block' ;
+  document.getElementById ('error').style.display = 'none' ;
+  }
+  else {
+    document.getElementById ('expenses-text').style.display = 'none' ;
+    document.getElementById ('error').style.display = 'block' ;
+   
+  }
   const balance =  document.getElementById ('balance') ;
-  const balanceText = balance.innerText ;
+  // total balance part 
+  if (incomeAmount > expensesTotal) {
   const balanceTotal = incomeAmount - expensesTotal ;
   balance.innerText = balanceTotal ;
+  document.getElementById ('balance-text').style.display = 'block' ;
+  document.getElementById ('error').style.display = 'none' ;
+  }
+  else {
+    document.getElementById ('expenses-text').style.display = 'none' ;
+    document.getElementById ('balance-text').style.display = 'none' ;
+    document.getElementById ('error').style.display = 'block' ;
+  }
 })
 // saving part 
 document.getElementById ('save-btn').addEventListener('click' , function (){
   const savingInput = inputValue('saveing-input') ;
-  console.log (savingInput)
   const incomeAmount = inputValue('income') ;
-  console.log (incomeAmount)
   const balanceText  =  document.getElementById ('balance').innerText
   const balanceAmount  = parseFloat(balanceText)
-  const savingAmount = incomeAmount*savingInput/100 ;
-  document.getElementById ('saving-amount').innerText = savingAmount ;
-  const remainingBalance = balanceAmount - savingAmount
-  document.getElementById ('Remaining-balance').innerText = remainingBalance
+  const savingAmountTotal = incomeAmount*savingInput/100 ;
+  document.getElementById ('saving-amount').innerText = savingAmountTotal ;
+  // const savingAmount = savingAmountTotal ;
+
+  if (savingAmountTotal < balanceAmount){
+    const remainingBalance = balanceAmount - savingAmountTotal ;
+    document.getElementById ('Remaining-balance').innerText = remainingBalance ;
+    document.getElementById ('remaining-error').style.display = 'none' ;
+    document.getElementById ('remaining-text').style.display = 'block' ;
+    document.getElementById ('saving-text').style.display = 'block' ;
+  }
+  else{
+    document.getElementById ('remaining-error').style.display = 'block' ;
+    document.getElementById ('remaining-text').style.display = 'none' ;
+    document.getElementById ('saving-text').style.display = 'none' ;
+  }
   
 })
 
