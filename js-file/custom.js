51d24fa745070cbc887a30cd4877aa2301ffb68e
Hiramon Nahar Eva <96996000+hiramonnahareva@users.noreply.github.com>
2022-02-16 23:32:00 -0800

    add saving part

--- js-file/custom.js
+++ js-file/custom.js
@@ -6,24 +6,38 @@ function inputValue (inputId){
   const Amount = parseFloat(AmountText)
   return Amount
 }
+// function ()
 
 document.getElementById ('calc-btn').addEventListener('click' ,function (){
- const incomeamount =  inputValue('income')
- const foodamount  =  inputValue ('food')
- const rentamount =  inputValue('rent')
- const clothesamount =  inputValue('clothes')
+ const incomeAmount =  inputValue('income')
+ const foodAmount  =  inputValue ('food')
+ const rentAmount =  inputValue('rent')
+ const clothesAmount =  inputValue('clothes')
 //  document.getElementById ('total-expeses').innerText = foodamount + rentamount + clothesamount ;
 
-
+//  function balance(){
   const expensesText = document.getElementById ('total-expeses');
-  const expensesAmount = parseFloat (expensesText)
-  const expensesTotal = foodamount + rentamount + clothesamount;
+  const expensesAmount = parseFloat (expensesText) 
+  const expensesTotal = foodAmount + rentAmount + clothesAmount; 
   expensesText.innerText = expensesTotal ;
-  
-//   document.getElementById ('balance').innerText = parseFloat(expensesTotal.innerText) - expensesAmount ;
-
-//   expense.innerText = rentValue +foodValue + clothesValue
-//   const totalExpenses = expanses
-//   const Expanses = rentValue +foodValue + clothesValue ;
-
+  const balance =  document.getElementById ('balance') ;
+  const balanceText = balance.innerText ;
+  const balanceTotal = incomeAmount - expensesTotal ;
+  balance.innerText = balanceTotal ;
 })
+// saving part 
+document.getElementById ('save-btn').addEventListener('click' , function (){
+  const savingInput = inputValue('saveing-input') ;
+  console.log (savingInput)
+  const incomeAmount = inputValue('income') ;
+  console.log (incomeAmount)
+  const balanceText  =  document.getElementById ('balance').innerText
+  const balanceAmount  = parseFloat(balanceText)
+  const savingAmount = incomeAmount*savingInput/100 ;
+  document.getElementById ('saving-amount').innerText = savingAmount ;
+  const remainingBalance = balanceAmount - savingAmount
+  document.getElementById ('Remaining-balance').innerText = remainingBalance
+  
+})
+
+
